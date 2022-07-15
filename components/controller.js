facc9f16bdec7b49ceb47a501655fa583ba6a1b9
Kim Vale <kimklintonvale@gmail.com>
2022-07-15 21:00:13 +0800

    Update controller.js

--- components/controller.js
+++ components/controller.js
@@ -121,44 +121,30 @@ const ParkingControl = () => {
             return false
         }
         
-        const new_arr = arr.map((obj)=>{
-            if (obj.id === selectedSlot.entrance) {
-                return {...obj, slots: (
-                    obj.slots.map(slot => {
-                        if (slot.id === selectedSlot.id) {
-                            return {...slot, is_vacant: true, time_in: 0, bill: 0}
-                        }
-                        return slot
-                    })
-                )}
+        Swal.fire({
+            icon: 'info',
+            title: `Entrance ${selectedSlot.entrance}, Slot #${selectedSlot.id}'s Invoice!`,
+            html: `<span>You have to pay PHP${selectedSlot.bill}!</span>`,
+            confirmButtonText: 'Ok',
+            showCancelButton: true,
+          }).then((result) => {
+            if (result.isConfirmed) {
+                const new_arr = arr.map((obj)=>{
+                    if (obj.id === selectedSlot.entrance) {
+                        return {...obj, slots: (
+                            obj.slots.map(slot => {
+                                if (slot.id === selectedSlot.id) {
+                                    return {...slot, is_vacant: true, time_in: 0, bill: 0}
+                                }
+                                return slot
+                            })
+                        )}
+                    }
+                    return obj
+                })
+                setParkingMap(new_arr)
             }
-            return obj
-        })
-        setParkingMap(new_arr)
-        // Swal.fire({
-        //     icon: 'info',
-        //     title: `Entrance ${selectedSlot.entrance}, Slot #${selectedSlot.id}'s Invoice!`,
-        //     html: `<span>You have to pay PHP${selectedSlot.bill}!</span>`,
-        //     confirmButtonText: 'Ok',
-        //     showCancelButton: true,
-        //   }).then((result) => {
-        //     if (result.isConfirmed) {
-        //         const new_arr = arr.map((obj)=>{
-        //             if (obj.id === selectedSlot.entrance) {
-        //                 return {...obj, slots: (
-        //                     obj.slots.map(slot => {
-        //                         if (slot.id === selectedSlot.id) {
-        //                             return {...slot, is_vacant: true, time_in: 0, bill: 0}
-        //                         }
-        //                         return slot
-        //                     })
-        //                 )}
-        //             }
-        //             return obj
-        //         })
-        //         setParkingMap(new_arr)
-        //     }
-        //   })
+          })
     }
 
     const updateBilling = (slots, selectedSlot, timeIn) => {
